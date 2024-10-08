8b98ed4c708c9beb1719f6b5fb1e3aab7053ab50
anurag-kochar <anuragmarketing101@gmail.com>
2024-10-08 07:45:45 +0530

    ⚒  remove: clerk auth

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,27 +1,16 @@
 import React from "react";
-import { UserButton, SignUpButton, currentUser } from "@clerk/nextjs";
 import { Button } from "../ui/button";
 import { Logo } from "../branding/logo";
 import { LogoWithText } from "../branding/logo-with-text";
-import { ThemeToggle } from "../theme-toggle";
 
-export const Header = async () => {
-  const user = await currentUser();
+export const Header = () => {
   return (
     <header className="w-full h-16 sticky top-0 left-0 right-0 flex justify-between items-center px-2 md:px-4 lg:px-6 border-b-2 bg-background">
       <Logo className="md:hidden" />
       <LogoWithText className="hidden md:flex" />
 
       <div className="flex justify-center items-center gap-4">
-        {/* <ThemeToggle /> */}
-        <Button variant={"secondary"}>Create an empty board</Button>
-        {!user ? (
-          <SignUpButton mode="modal">
-            <Button>Get started</Button>
-          </SignUpButton>
-        ) : (
-          <UserButton afterSignOutUrl="/" />
-        )}
+        <Button>Create an empty board</Button>
       </div>
     </header>
   );
